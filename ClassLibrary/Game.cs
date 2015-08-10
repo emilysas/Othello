3eaebc5821ac96788f7a7b7c5b5d89018345d01d
Emily Sas <emilysas@users.noreply.github.com>
2015-08-10 12:40:08 +0100

    game passing two further tests - a player skips their turn if they pass, if both players pass the game ends

--- ClassLibrary/Game.cs
+++ ClassLibrary/Game.cs
@@ -4,13 +4,16 @@
     {
         private readonly Player<Counter> _player1;
         private readonly Player<Counter> _player2;
-        private Player<Counter> _playerWhosTurnItIs; 
+        private Player<Counter> _playerWhosTurnItIs;
+        private bool _isFinished;
+        private int _passCount;
 
         public Game(Player<Counter> player1, Player<Counter> player2)
         {
             _player1 = player1;
             _player2 = player2;            
             _playerWhosTurnItIs = _player1;
+            _passCount = 0;
         }
 
         public Player<Counter> PlayerToPlayNext()
@@ -18,10 +21,22 @@
             return _playerWhosTurnItIs;
         }
 
+        public bool IsFinished()
+        {
+            return _passCount > 1;
+        }
+
         public void Play(string gridRef)
         {
             _playerWhosTurnItIs.Play(gridRef);
+            _passCount = 0;
             NextPlayersTurn();
+        }
+
+        public void Pass()
+        {
+            NextPlayersTurn();
+            _passCount += 1;
         }
 
         private void NextPlayersTurn()
--- NUnitTests/GameTests.cs
+++ NUnitTests/GameTests.cs
@@ -8,30 +8,44 @@ namespace NUnitTests
     public class GameTests
     {
        
+        private Player<Counter> player1;
+        private Player<Counter> player2;
+        private OthelloBoard board;
+        private Game game;
+
+        [SetUp]
+        public void Init()
+        {
+            board = new OthelloBoard();
+            player1 = new Player<Counter>("Emily", board, "black");
+            player2 = new Player<Counter>("Berry", board, "white");
+            game = new Game(player1, player2);
+        }
+
         [Test]
         public void PlayersTakeTurnsToPlay()
         {
-            var board = new OthelloBoard();
-            var player1 = new Player<Counter>("Emily", board, "black");
-            var player2 = new Player<Counter>("Berry", board, "white");
-            var game = new Game(player1, player2);
-
             Assert.That(game.PlayerToPlayNext(), Is.EqualTo(player1));
             game.Play("C4");
             Assert.That(game.PlayerToPlayNext(), Is.EqualTo(player2));
         }
 
-//        [Test]
-//        public void IfAPlayerPassesThenTheySkipTheirTurn()
-//        {
-//
-//        }
-//
-//        [Test]
-//        public void IfBothPlayersPassTheGameWillEnd()
-//        {
-//
-//        }
+        [Test]
+        public void IfAPlayerPassesThenTheySkipTheirTurn()
+        {
+            Assert.That(game.PlayerToPlayNext(), Is.EqualTo(player1));
+            game.Pass();
+            Assert.That(game.PlayerToPlayNext(), Is.EqualTo(player2));
+        }
+
+        [Test]
+        public void IfBothPlayersPassTheGameWillEnd()
+        {
+            Assert.That(game.IsFinished(), Is.EqualTo(false));
+            game.Pass();
+            game.Pass();
+            Assert.That(game.IsFinished(), Is.EqualTo(true));
+        }
 //
 //        [Test]
 //        public void TheGameWillEndWhenTheBoardIsFull()
